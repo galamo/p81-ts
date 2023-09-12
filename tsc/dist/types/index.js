24e5e7084d1a765b89a65d8d69311f9bc32cfed5
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2023-09-12 11:40:48 +0300

    ex

--- tsc/dist/types/index.js
+++ tsc/dist/types/index.js
@@ -8,6 +8,22 @@ var licenseTo = new Date().toISOString();
 var temp;
 var myNallabaleItem;
 console.log("The product is: ".concat(productName, " it will cost : ").concat(cost, ", \n  available from: ").concat(licenseFrom, " to: ").concat(licenseTo, " is the product valuable ? ").concat(usefull, " "));
+var business = "security";
+console.log(business);
+var products = ["P1", "P2", "P3"];
+var result = products.map(function (item) {
+    return { name: item, type: "product" };
+});
+var salaries = [100, 222, 3300, 4400];
+var highSalaries = salaries.reduce(function (sal, currentSalary) {
+    if (currentSalary > 100) {
+        return sal + currentSalary;
+    }
+    else
+        return sal;
+}, 0);
+highSalaries.toLowerCase();
+console.log(highSalaries);
 // never
 function doSomething() { }
 exports.default = doSomething;
--- tsc/src/types/index.ts
+++ tsc/src/types/index.ts
@@ -1,4 +1,4 @@
-const productName: string = "Perimeter81";
+let productName: string = "Perimeter81";
 const cost: number = 100000;
 const usefull: boolean = true;
 const licenseFrom: Date = new Date();
@@ -11,6 +11,23 @@ console.log(
   `The product is: ${productName} it will cost : ${cost}, 
   available from: ${licenseFrom} to: ${licenseTo} is the product valuable ? ${usefull} `
 );
+
+let business = "security";
+console.log(business);
+
+const products = ["P1", "P2", "P3"];
+const result = products.map((item) => {
+  return { name: item, type: "product" };
+});
+
+const salaries = [100, 222, 3300, 4400];
+const highSalaries = salaries.reduce((sal: any, currentSalary: any) => {
+  if (currentSalary > 100) {
+    return sal + currentSalary;
+  } else return sal;
+}, 0);
+highSalaries.toLowerCase();
+console.log(highSalaries);
 
 // never
 
