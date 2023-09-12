c4d3e48468388c0aabbfe9ac8c6b406010404023
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2023-09-12 10:51:35 +0300

    concurrently

---
+++ tsc/dist/index.js
@@ -0,0 +1,4 @@
+var user = "galamouyal88@gmail.com";
+var age = 34;
+console.log(user, age);
+// console.log(age.toLweCase());
---
+++ tsc/src/index.ts
@@ -0,0 +1,4 @@
+const user: string = "galamouyal88@gmail.com";
+const age: number = 34;
+console.log(user, age);
+// console.log(age.toLweCase());
