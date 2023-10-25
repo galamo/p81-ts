b78562357e23460da4543b53d3cf6ee3844783c5
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2023-10-25 10:50:20 +0300

    module-section

---
+++ index.js
@@ -0,0 +1,9 @@
+// start of project
+
+async function getData() {
+  return 1;
+}
+
+const result = await getData();
+
+// rest of the project
--- nodejs-module-esm/index.mjs
+++ nodejs-module-esm/index.mjs
@@ -1,3 +1,15 @@
 import { getStats } from "./lib.mjs";
 
 console.log(getStats());
+
+async function getDate() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      return resolve("DB connected, initial cache pulled ");
+    }, 3000);
+  });
+}
+
+const result = await getDate();
+console.log(result);
+console.log("This is the end of loading the api...");
